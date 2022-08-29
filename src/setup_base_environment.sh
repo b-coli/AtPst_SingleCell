8eb1ccbf8ec7680771c84e37e04fc340c9a142dd
Benjamin Jeremy Cole <BJCole@lbl.gov>
2022-08-29 15:50:18 -0700

    added file metadata

---
+++ src/setup_base_environment.sh
@@ -0,0 +1,37 @@
+sudo apt-get update
+sudo apt-get install -y \
+  build-essential \
+  libseccomp-dev \
+  libglib2.0-dev \
+  pkg-config \
+  squashfs-tools \
+  cryptsetup runc
+
+export VERSION=1.18.4 OS=linux ARCH=amd64  # change this as you need
+
+wget -O /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz \
+  https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
+
+sudo tar -C /usr/local -xzf /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
+
+git clone --recurse-submodules https://github.com/sylabs/singularity.git
+
+cd singularity
+
+git checkout --recurse-submodules v3.10.2
+
+./mconfig
+make -C builddir
+sudo make -C builddir install
+
+cd ~/
+
+singularity pull docker://bcoli/atpst_singlecell_r:1.0
+singularity pull docker://dceoy/gffread:latest
+singularity pull docker://cumulusprod/cellranger:6.0.1
+singularity pull docker://mparikhbroad/velocyto:1.0.1
+singularity pull docker://ncbi/sra-tools:3.0.0
+
