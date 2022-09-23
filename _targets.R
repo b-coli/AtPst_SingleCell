f8f89e552d299a191a91fdcec23e4d87906ad50d
b-coli <bjcole@atpst-singlecell-highmem.c.regal-marking-269515.internal>
2022-09-23 21:55:30 +0000

    added cluster renaming and log files

--- _targets.R
+++ _targets.R
@@ -7,76 +7,27 @@ library(future)
 library(monocle3)
 
 source("src/target_functions.R")
+future::plan(strategy = "multisession", workers = 32)
 
 sample_metadata <- readr::read_csv("metadata/sample_metadata.csv")
 
 bulk_data <- list(
-  tar_target(
-    bulk_dge, read_bulk_dges(
-      sns = sample_metadata %>%
-        filter(Sequence_Type == "RNAseq") %>%
-        pull(Sample_Name)
-    )
-  ),
-  tar_target(
-    bulk_erobj_norm, 
-    generate_er_object(
-      dge = bulk_dge, 
-      md = filter(sample_metadata, Sequence_Type == "RNAseq")
-    )
-  ),
-  
-  tar_target(
-    bulk_de_table,
-    get_de_genes(bulk_erobj_norm)
-  ),
-  
-  tar_target(
-    protoplast_loci,
-    bulk_de_table %>%
-      filter(de_type_proto %in% c("Proto Up", "Proto Down")) %>% 
-      pull(Locus)
-  ),
-  
-  tar_render(
-    bulk_analysis_report, 
-    path = "src/bulk_analysis_report.Rmd", 
-    output_dir = "reports/")
+  tar_target(bulk_dge, read_bulk_dges(sns = sample_metadata %>% filter(Sequence_Type == "RNAseq") %>% pull(Sample_Name))),
+  tar_target(bulk_erobj_norm, generate_er_object(dge = bulk_dge, md = filter(sample_metadata, Sequence_Type == "RNAseq"))),
+  tar_target(bulk_de_table, get_de_genes(bulk_erobj_norm)),
+  tar_target(protoplast_loci, bulk_de_table %>% filter(de_type_proto %in% c("Proto Up", "Proto Down")) %>% pull(Locus)),
+  tar_render(bulk_analysis_report, path = "src/bulk_analysis_report.Rmd", output_dir = "reports/")
 )
 
 individual_objects <- list(
-  tar_target(
-    leaf_ref, 
-    build_reference_dataset(path = "data/expression/KimPublished")
-  ),
-  
+  tar_target(leaf_ref, build_reference_dataset(path = "data/expression/KimPublished")),
   tar_map(
     values = filter(sample_metadata, Sequence_Type == "scRNAseq"),
     names = "Sample_Name",
     unlist = F,
-    tar_target(
-      loom_file,
-      paste0("data/expression/", Sample_Name, "/velocyto/", Sample_Name, ".loom"),
-      format = "file"
-    ),
-    
-    tar_target(
-      raw_sobj,
-      loom_to_sobj(
-        loom_file = loom_file,
-        md_file = Cell_Metadata_File,
-        sample_name = Sample_Name
-      )
-    ),
-    
-    tar_target(
-      sobj, 
-      process_sobj(
-        sobj = raw_sobj, 
-        ref = leaf_ref,
-        de_table = bulk_de_table
-      )
-    )
+    tar_target(loom_file, paste0("data/expression/", Sample_Name, "/velocyto/", Sample_Name, ".loom"), format = "file"),
+    tar_target(raw_sobj, loom_to_sobj(loom_file = loom_file, md_file = Cell_Metadata_File, sample_name = Sample_Name)),
+    tar_target(sobj, process_sobj(sobj = raw_sobj, ref = leaf_ref, de_table = bulk_de_table))
   )
 )
 
@@ -108,11 +59,9 @@ integrated_objects <- list(
     )
   ),
   
-  tar_render(
-    pseudobulk_report, 
-    path = "src/pseudobulk_report.Rmd", 
-    output_dir = "reports/")
-  
+  tar_target(cluster_deg, FindAllMarkers(archived_sobj, assay = "RNA", logfc.threshold = 0, min.pct = 0)),
+  tar_render(pseudobulk_report, path = "src/pseudobulk_report.Rmd", output_dir = "reports/"),
+  tar_render( dataset_stats_report, path = "src/dataset_stats_report.Rmd", output_dir = "reports/")
 )
 
 pseudotime_objects <- list(
@@ -135,74 +84,18 @@ pseudotime_objects <- list(
       setdiff(x = row.names(archived_sobj@assays$SCT))
   ),
   
-  tar_target(
-    atpst_mobj,
-    monocle_pipeline(
-      sobj = archived_sobj,
-      cells = monocle_cells,
-      features = monocle_genes,
-      gene_ids = gene_ids
-    )
-  ),
-  
-  tar_target(
-    pt_var_genes,
-    monocle_get_var_genes(atpst_mobj)
-  ),
-  
-  tar_render(
-    pseudotime_report, 
-    path = "src/pseudotime_report.Rmd", 
-    output_dir = "reports/")
-  
-  
+  tar_target(atpst_mobj, monocle_pipeline(sobj = archived_sobj, cells = monocle_cells, features = monocle_genes, gene_ids = gene_ids)),
+  tar_target(pt_var_genes, monocle_get_var_genes(atpst_mobj)),
+  tar_render(pseudotime_report, path = "src/pseudotime_report.Rmd", output_dir = "reports/")
 )
 
 archival_data <- list(
-  tar_target(
-    gene_ids_file,
-    "data/gene_aliases_20200930.txt.gz",
-    format = "file"
-  ),
-
-  tar_target(
-    archived_sobj_file,
-    "data/GSE213622_coaker_atpst_singlecell_sobj.rds",
-    format = "file"
-  ),
-
-  tar_target(
-    gene_ids,
-    read_tsv(
-      gene_ids_file,
-      col_types = cols(),
-      col_names = c("Locus", "Gene", "Full")
-    )
-  ),
-
-  tar_target(
-     archived_sobj,
-     read_archived_sobj(
-       fn = archived_sobj_file,
-       cluster_names = archived_clusterNames)
-  ),
-  
-  tar_target(
-    archived_clusterNames_file,
-    "data/cluster_renaming.csv",
-    format = "file"
-  ),
-  
-  tar_target(
-    archived_clusterNames,
-    read_csv(archived_clusterNames_file)
-  ),
-  
-  tar_render(
-    dataset_stats_report, 
-    path = "src/dataset_stats_report.Rmd", 
-    output_dir = "reports/")
-  
+  tar_target(gene_ids_file, "data/gene_aliases_20200930.txt.gz", format = "file"),
+  tar_target(gene_ids, read_tsv(gene_ids_file, col_types = cols(), col_names = c("Locus", "Gene", "Full"))),
+  tar_target(archived_sobj_file, "data/GSE213622_coaker_atpst_singlecell_sobj.rds", format = "file"),
+  tar_target(archived_sobj, read_archived_sobj(fn = archived_sobj_file, cluster_names = archived_clusterNames)),
+  tar_target(archived_clusterNames_file, "data/cluster_renaming.csv", format = "file"),
+  tar_target(archived_clusterNames, read_csv(archived_clusterNames_file))
 )
 
 list(
